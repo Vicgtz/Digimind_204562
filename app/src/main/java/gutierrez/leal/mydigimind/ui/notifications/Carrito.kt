2437a2a9176d710c4d1306cc7d53eed0bd3f75e7
Vicgtz09 <65575428+Vicgtz09@users.noreply.github.com>
2021-03-21 21:34:36 -0700

    pantallas

---
+++ app/src/main/java/gutierrez/leal/mydigimind/ui/notifications/Carrito.kt
@@ -0,0 +1,11 @@
+package gutierrez.leal.mydigimind.ui.notifications
+
+import java.io.Serializable
+
+class Carrito :Serializable {
+    var recordatorios = ArrayList<Recordatorio>()
+
+    fun agregar(p: Recordatorio): Boolean{
+        return recordatorios.add(p)
+    }
+}
